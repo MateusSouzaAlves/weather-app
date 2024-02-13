20348db59dbf5a9e26f40e7e49af12c6b23b321f
MateusSouzaAlves <javaamoavida@gmail.com>
2024-02-13 17:01:43 -0300

    Projeto Angular

---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private apiKey= 'b6e7a3a6b3b49074829bc3e777eae86b';
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherDatas(cityName : string): Observable<any> {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?',{})
+  }
+}
